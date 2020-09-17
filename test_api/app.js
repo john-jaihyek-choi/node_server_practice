848aa8affb5a63e32985a3774a37894e50535a95
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-09-16 22:13:23 -0700

    started setting end points for the post method

--- test_api/app.js
+++ test_api/app.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const fortunes = require('./data/fortunes.json')
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.get('/fortunes', (req, res) => {
 
     res.json(fortunes);
 })
+
+app.get('/fortunes/random', (req, res) => {
+    res.json(fortunes[Math.floor(Math.random() * fortunes.length)]);
+})
+
+app.get('/fortunes/:id', (req, res) => {
+    res.json(fortunes.find( fortune => fortune.id == req.params.id))
+})
+
+app.post('/fortunes', (req, res) => {
+    
+    console.log(req.body);
+})
+
 
 module.exports = app;
